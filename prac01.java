8a5879b8b52cbbe90f31f0893e132fc97a3faed1
wutree0716 <mstl0716@gmail.com>
2018-05-06 08:34:04 +0800

    New prct01.java
    New practise01 java file

---
+++ prac01.java
@@ -0,0 +1,9 @@
+package com.prac;
+
+public class prac01 {
+	
+	public static void main(String[] args) {
+	    // TODO Auto-generated method stub
+	    System.out.println("Hello World!"); //請自行輸入此行
+	  }
+}
